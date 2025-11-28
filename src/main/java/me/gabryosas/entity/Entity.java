4c1735ac1d3581e460f13d7f9e07bbeea0a0f12d
GabryOsas <giggigiochicollaboro@gmail.com>
2025-11-28 20:15:41 +0100

    Adding new stuff

--- src/main/java/me/gabryosas/entity/Entity.java
+++ src/main/java/me/gabryosas/entity/Entity.java
@@ -1,7 +1,16 @@
 package me.gabryosas.entity;
 
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Entity {
     public int x, y;
     public int speed;
 
+    public List<BufferedImage> images = new ArrayList<>();
+    public String direction;
+
+    public int spriteCounter = 0;
+    public int spriteNum = 1;
 }
--- src/main/java/me/gabryosas/entity/Player.java
+++ src/main/java/me/gabryosas/entity/Player.java
@@ -3,42 +3,100 @@ package me.gabryosas.entity;
 import me.gabryosas.GamePanel;
 import me.gabryosas.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Player extends Entity {
     GamePanel gp;
     KeyHandler keyH;
+    String[] names = {"down",  "up", "right", "left", "idle"};
 
     public Player(GamePanel gp, KeyHandler keyH){
         this.gp = gp;
         this.keyH = keyH;
+
+        setDefaultValues();
+        getPlayerImage();
     }
     public  void setDefaultValues(){
         x = 100;
         y = 100;
         speed = 4;
+        direction = "down";
     }
+
+    public void getPlayerImage(){
+        try {
+            int animation = 2;
+            for (int i = 0, j = 0, z = 0, y = 1; i < names.length * animation; i++) {
+                images.add(ImageIO.read(getClass().getResourceAsStream("/player/boy_" + names[j] + "_" + y + ".png")));
+                z++;
+                y++;
+                if (z == 2) {
+                    j++;
+                    z = 0;
+                }
+                if (y == 3) {
+                    y = 1;
+                }
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
     public void update(){
 
         /*Y se aumanta va sotto perché la coordinata di base si trova a (0; 0)
         X aumenta va a destra*/
 
         if (keyH.upPressed) {
-           x-= speed;
-        }
-        if (keyH.downPressed) {
+            direction = "up";
+           y -= speed;
+        } else if (keyH.downPressed) {
+            direction = "down";
+            y += speed;
+        } else if (keyH.rightPressed) {
+            direction = "right";
             x += speed;
-        }
-        if (keyH.rightPressed) {
-            x += speed;
-        }
-        if (keyH.leftPressed) {
+        } else if (keyH.leftPressed) {
+            direction = "left";
             x -= speed;
+        } else {
+            direction = "idle";
+        }
+
+        spriteCounter++;
+        if (spriteCounter > 12){
+            if (spriteNum == 1) {
+                spriteNum = 2;
+            } else if (spriteNum == 2) {
+                spriteNum = 1;
+            }
+            spriteCounter = 0;
         }
     }
     public void draw(Graphics2D g2){
-        g2.setColor(Color.WHITE);
+        /*g2.setColor(Color.WHITE);
+        g2.fillRect(x, y, gp.tileSize, gp.tileSize);*/
 
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        BufferedImage image = null;
+
+        int[] values = new int[2];
+        for (int i = 0; i < names.length; i++) {
+            if (direction.equalsIgnoreCase(names[i])){
+                values[0] = i * 2;
+                values[1] = (i * 2) + 1;
+            }
+        }
+        if (spriteNum == 1) {
+            image = images.get(values[0]);
+        } else {
+            image = images.get(values[1]);
+        }
+
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
     }
 }
